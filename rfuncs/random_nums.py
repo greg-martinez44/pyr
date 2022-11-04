d11211eb7bd9c9051e851dd17d7cf16f0dfd9f41
Greg Martinez <greg.martinez44@gmail.com>
2022-11-04 10:44:20 -0700

    added typing and docstrings

--- rfuncs/random_nums.py
+++ rfuncs/random_nums.py
@@ -2,9 +2,11 @@ import numpy as np
 
 SEED = None
 
-def set_seed(new_seed):
+def set_seed(new_seed: int) -> None:
+    """Sets the seed for random value generation."""
     global SEED
     SEED = new_seed
 
-def rnorm(size, mean=0, sd=1):
+def rnorm(size:int, mean:int=0, sd:int=1) -> np.ndarray:
+    """Creates a random array of integers with given mean and standard deviation."""
     return np.random.default_rng(SEED).normal(loc=mean, scale=sd, size=size)
