b4f6a96bca66b8d0509e5d9e57b2b5b74649fb11
Greg Martinez <greg.martinez44@gmail.com>
2022-11-04 10:31:22 -0700

    added typing and docstrings

--- rfuncs/data.py
+++ rfuncs/data.py
@@ -1,7 +1,10 @@
+from typing import List
 import pandas as pd
 
-def read_csv(io):
-    return pd.read_csv(io)
+def read_csv(io: str, *args, **kwargs) -> pd.DataFrame:
+    """Reads csv file and returns a dataframe."""
+    return pd.read_csv(io, *args, **kwargs)
 
-def names(df):
+def names(df: pd.DataFrame) -> List[str]:
+    """Returns a list of column names from df."""
     return df.columns.tolist()
