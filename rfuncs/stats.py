5b2b13d15d2724affa70ba6f0440b269f9b8e068
Greg Martinez <greg.martinez44@gmail.com>
2022-11-04 10:52:01 -0700

    added typing and docstrings

--- rfuncs/stats.py
+++ rfuncs/stats.py
@@ -1,19 +1,25 @@
 import numpy as np
 
-def exp(arr, exp):
+def exp(arr:np.ndarray, exp:int) -> np.ndarray:
+    """Returns an array with all elements raised to exp."""
     return np.power(arr, exp)
 
-def sqrt(arr):
+def sqrt(arr:np.ndarray) -> np.ndarray:
+    """Returns an array with the square root of each element."""
     return np.sqrt(arr)
 
-def cor(x, y):
+def cor(x:np.ndarray, y:np.ndarray) -> np.ndarray:
+    """Returns the correlation coefficient from two arrays."""
     return np.corrcoef(x, y)[0][1]
 
-def mean(arr):
+def mean(arr:np.ndarray) -> float:
+    """Returns the mean of an array."""
     return np.mean(arr)
 
-def var(arr):
+def var(arr:np.ndarray) -> float:
+    """Returns the variance of an array."""
     return np.var(arr)
 
-def sd(arr):
+def sd(arr:np.ndarray) -> float:
+    """Returns the standard deviation of an array."""
     return np.std(arr)
