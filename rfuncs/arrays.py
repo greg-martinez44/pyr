681cf38e895deb83385c1cc95d5c1c2c854f5207
Greg Martinez <greg.martinez44@gmail.com>
2022-11-04 10:20:55 -0700

    added typing and docstrings

--- rfuncs/arrays.py
+++ rfuncs/arrays.py
@@ -1,12 +1,16 @@
+from typing import Optional, Tuple
 import numpy as np
 
-def c(*args):
+def c(*args: int) -> np.ndarray:
+    """Creates an array from a sequence of integers."""
     return np.array([x for x in args])
 
-def length(array):
+def length(array: np.ndarray) -> int:
+    """Returns the length of an array."""
     return len(array)
 
-def matrix(array, nrow=1, ncol=1, byrow=False):
+def matrix(array: np.ndarray, nrow:int=1, ncol:int=1, byrow:bool=False) -> np.ndarray:
+    """Creates a nrow x ncol matrix from a given array."""
     array_length = nrow * ncol
     if len(array) < array_length:
         array = np.tile(array, array_length+len(array))
@@ -24,12 +28,14 @@ def matrix(array, nrow=1, ncol=1, byrow=False):
         matrix.append(this_row)
     return np.asarray(matrix)
 
-def seq(lower, upper, length=None):
+def seq(lower:int, upper:int, length:Optional[int]=None) -> np.ndarray:
+    """Creates an array with values from lower to upper."""
     if length:
         step = (upper - lower) / length
         return np.linspace(lower, upper, num=length, endpoint=False)
     return np.arange(lower, upper+1)
 
 
-def dim(array):
+def dim(array:np.ndarray) -> Tuple[int]:
+    """Returns the shape of the array."""
     return array.shape
